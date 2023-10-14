3880793bb56439b90e1c620a570ed2f7c64d1af2
Sudip Paik <45372530+sudipiitkgp@users.noreply.github.com>
2023-10-14 08:44:04 +0530

    This is my first github push

---
+++ mongo.py
@@ -0,0 +1,12 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://sudippaik:usuitakumi@cluster0.v3yyqhf.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d = {
+    "name":"sudip",
+    "email" : "sudip@gmail.com",
+    "surname" : "paik"
+}
+db1 = client['mongo']
+coll = db1['test']
+coll.insert_one(d )
